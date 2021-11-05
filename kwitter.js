149b7e184c017ba649107aaa7e59c51e61373cd2
Anushka1227 <83247289+Anushka1227@users.noreply.github.com>
2021-11-05 16:41:51 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,5 @@
+function addUser()
+{
+    var addUser = document.getElementById("addUser").value;
+    localStorage.setItem("addUser", addUser); 
+}
---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAL71aNsspVHUtpDYgGdQr3kUnFXj_Dcyk",
+    authDomain: "project-94-bb5eb.firebaseapp.com",
+    databaseURL: "https://project-94-bb5eb-default-rtdb.firebaseio.com",
+    projectId: "project-94-bb5eb",
+    storageBucket: "project-94-bb5eb.appspot.com",
+    messagingSenderId: "76539077865",
+    appId: "1:76539077865:web:6633c59671fe042d3bbc78",
+    measurementId: "G-NTMKCTNFF2"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
